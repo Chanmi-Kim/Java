d37cbcfc30e71324dfffa5065a5468d1321c48db
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 17:34:08 +0900

    Add Folder [[2019.02.08]오버로딩]
    1. 폴더 및 JAVA 파일 추가
    
    - Ex21_11.java
    - Ex21_12.java
    - Ex21_13.java

---
+++ 제출/[2019.02.08]오버로딩/Ex21_13.java
@@ -0,0 +1,93 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.08.
+
+요구사항13. 인자로 받은 숫자들 중 양수의 갯수를 반환하는 메소드를 선언하고 호출하시오.
+
+출력]
+양수 : 4개
+
+*/
+
+class Ex21_13 
+{
+	public static void main(String[] args) 
+	{
+		int count = positive(10, 20, -30, 40, 50);
+		System.out.printf("양수 : %d개\n\n", count);
+	}
+
+	public static int positive(int n1)
+	{
+		int count = n1 < 0 ? 0 : 1;
+
+		return count;
+	}
+
+	public static int positive(int n1, int n2)
+	{
+		int result = 0;
+		int count = n1 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n2 < 0 ? 0 : 1;
+		result = result + count;
+
+		return result;
+	}
+
+	public static int positive(int n1, int n2, int n3)
+	{
+		int result = 0;
+		int count = n1 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n2 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n3 < 0 ? 0 : 1;
+		result = result + count;
+
+		return result;
+	}
+
+	public static int positive(int n1, int n2, int n3, int n4)
+	{
+		int result = 0;
+		int count = n1 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n2 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n3 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n4 < 0 ? 0 : 1;
+		result = result + count;
+
+		return result;
+	}
+
+	public static int positive(int n1, int n2, int n3, int n4, int n5)
+	{
+		int result = 0;
+		int count = n1 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n2 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n3 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n4 < 0 ? 0 : 1;
+		result = result + count;
+
+		count = n5 < 0 ? 0 : 1;
+		result = result + count;
+
+		return result;
+	}
+}
