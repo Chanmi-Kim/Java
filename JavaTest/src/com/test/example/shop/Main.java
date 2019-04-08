d30bd086c57cf82aa4015d2bc893ddb34a2c6925
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 14:59:35 +0900

    Add Folder [shop]
    1. 폴더 및 JAVA 파일 추가
    
    - Main.java

---
+++ JavaTest/src/com/test/example/shop/Main.java
@@ -0,0 +1,24 @@
+package com.test.example.shop;
+
+import java.io.BufferedWriter;
+import java.io.File;
+
+public class Main {
+	
+//	private ArrayList<Product> plist;
+//	private ArrayList<Member> mlist;
+//	private ArrayList<Order> olist;
+//	
+	
+	public static void main(String[] args) {
+		
+//		File file = new File(path2);
+//		
+//		file.exists()
+//		
+//		BufferedWriter writer = new ();
+//		writer.write("aaa");
+		
+	}
+
+}
