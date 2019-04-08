b39a37dad8ee27dd8cd6510312242969af77b0dc
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 17:35:31 +0900

    Add Folder [[2019.02.08]오버로딩]
    1. 폴더 및 JAVA 파일 추가
    
    - Ex22_1.java
    - Ex22_2.java
    - Ex22_3.java
    - Ex22_4.java
    - Ex22_5.java

---
+++ 제출/[2019.02.11]날짜시간/Ex22_1.java
@@ -0,0 +1,36 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.08.
+
+요구사항1. 오늘 태어난 아이의 백일과 첫돌이 언제인지 출력하시오.
+
+출력]
+백일 : 2019-05-21
+첫돌 : 2020-02-10
+
+*/
+
+import java.util.Calendar;
+
+class Ex22_1 
+{
+	public static void main(String[] args) 
+	{
+		add();
+	}
+
+	public static void add()
+	{
+
+		Calendar today = Calendar.getInstance();
+	
+		today.add(Calendar.DATE, 100);
+		System.out.printf("백일 : %tF\n", today);
+
+		today = Calendar.getInstance(); // 초기화
+		today.add(Calendar.YEAR, 1);
+		System.out.printf("첫돌 : %tF\n", today);
+	}
+		
+}
---
+++ 제출/[2019.02.11]날짜시간/Ex22_2.java
@@ -0,0 +1,41 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.08.
+
+요구사항2. 태어난 년도를 입력하면 나이를 출력하시오.
+
+입력]
+태어난 년도 : 1995
+
+출력]
+나이 : 25세
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
+class Ex22_2 
+{
+	public static void main(String[] args) throws Exception
+	{
+		age();
+	}
+
+	public static void age() throws Exception
+	{
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		Calendar birthday = Calendar.getInstance();
+	
+		int thisYear = birthday.get(Calendar.YEAR); // 현재년도 추출
+
+		System.out.print("태어난 년도 : ");
+		String year = reader.readLine();
+		
+		int birthYear = Integer.parseInt(year);
+
+		System.out.println(thisYear - birthYear + 1);	
+	}
+}
