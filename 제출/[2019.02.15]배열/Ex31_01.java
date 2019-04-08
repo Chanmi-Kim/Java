d257e8a63b4230b27c1ac913f6b816cccc82b37a
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 17:42:13 +0900

    Add Folder [[2019.02.15]배열]
    1. 폴더 및 JAVA 파일 추가
    
    - Ex31_01.java
    - Ex31_02.java
    - Ex31_03.java
    - Ex31_04.java
    - Ex31_05.java
    - Ex31_06.java
    - Ex31_07.java

---
+++ 제출/[2019.02.15]배열/Ex31_01.java
@@ -0,0 +1,50 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.15.
+
+요구사항 1. 숫자 5개를 입력받아 배열에 담은 뒤 역순으로 출력하시오.
+
+입력]
+숫자 : 5
+숫자 : 1
+숫자 : 7
+숫자 : 2
+숫자 : 3
+
+출력]
+list[4] = 3
+list[3] = 2
+list[2] = 7
+list[1] = 1
+list[0] = 5
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex31_01 {
+
+	public static void main(String[] args) throws Exception{
+		numsOutput();
+	}
+
+	public static void numsOutput() throws Exception{
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int nums[] = new int[5];
+		
+		for(int i=0; i<nums.length; i++) {
+			System.out.print("숫자 : ");
+			nums[i] = Integer.parseInt(reader.readLine());
+		}
+
+		for(int i=nums.length-1; i>=0; i--) {
+			System.out.printf("list[%d] = %d\n", i, nums[i]);
+		}
+		
+	} // numsOutput
+
+} // Ex31_01
---
+++ 제출/[2019.02.15]배열/Ex31_02.java
@@ -0,0 +1,59 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.15.
+
+요구사항 2. 학생의 이름을 N개 입력받아 아래와 같이 출력하시오.
+
+입력]
+학생 수 : 6
+학생명 : 홍길동
+학생명 : 아무개
+학생명 : 하하하
+학생명 : 호호호
+학생명 : 후후후
+학생명 : 헤헤헤
+
+출력]
+입력한 학생은 총 6명입니다.
+1. 홍길동
+2. 아무개
+3. 하하하
+4. 호호호
+5. 후후후
+6. 헤헤헤
+
+추가조건]
+- 입력받은 학생 이름은 일단 동적 배열에 저장한다.
+- 입력받은 순서의 반대로 출력한다.
+
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex31_02 {
+	public static void main(String[] args) throws Exception {
+		nameOutput();
+	}
+
+	public static void nameOutput() throws Exception {
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("학생 수 : ");
+		int studentNum = Integer.parseInt(reader.readLine());
+
+		String studentName[] = new String[studentNum];
+
+		for (int i = 0; i < studentNum; i++) {
+			System.out.print("학생명 : ");
+			studentName[i] = reader.readLine();
+		}
+
+		System.out.printf("\n입력한 학생은 총 %d명입니다.\n", studentNum);
+		for (int i = studentNum - 1; i >= 0; i--) {
+			System.out.printf("%d. %s\n", studentNum - i, studentName[i]);
+		}
+	} // nameOutput
+} // Ex31_02
