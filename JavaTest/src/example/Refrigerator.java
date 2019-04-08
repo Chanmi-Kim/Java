0577102b6e8ab4f2b9963b223c89fbc571422d42
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 13:59:22 +0900

    Add File [Ex40_Class.java] Add File [Item.java] Add File [Refrigerator.java]
    1. JAVA 파일 추가
    
    - Ex40_Class.java
    - Item.java
    - Refrigerator.java

---
+++ JavaTest/src/example/Refrigerator.java
@@ -0,0 +1,29 @@
+package example;
+
+public class Refrigerator {
+	
+//	private Item2[] items = new Item2[100];
+//
+//	public void add(Item2 item) {
+//		
+//	}
+//	
+//	public Item2 get(String name) {
+//		
+//	}
+//	
+//	public int count() {
+//		
+//	}
+//	
+//	public void listItem() {
+//		
+//	}
+}
+
+
+
+
+
+
+
