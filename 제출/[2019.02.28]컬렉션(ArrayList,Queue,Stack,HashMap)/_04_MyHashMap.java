e79e71777933ffec3d8babcc4eb8ee94a6a702f2
xxHANIxx <kimcm0319@gmail.com>
2019-04-08 20:36:21 +0900

    Add File [_04_Ex77_HashMap.java] Add File [_04_MyHashMap.java]
    1. 폴더 및 JAVA 파일 추가
    
    - _04_Ex77_HashMap.java 파일 추가
      (제출\[2019.02.28]컬렉션(ArrayList,Queue,Stack,HashMap)\_04_Ex77_HashMap.java)
    - _04_MyHashMap.java 파일 추가
      (제출\[2019.02.28]컬렉션(ArrayList,Queue,Stack,HashMap)\_04_MyHashMap.java)

---
+++ 제출/[2019.02.28]컬렉션(ArrayList,Queue,Stack,HashMap)/_04_MyHashMap.java
@@ -0,0 +1,56 @@
+
+public class _04_MyHashMap {
+
+	public void put(String key, String value) {
+		// 키와 값을 요소로 추가한다.
+		// key : 추가할 요소의 키
+		// value : 추가할 요소의 값
+		
+	}
+	
+	public String get(String key) {
+		// 키에 대응하는 요소의 값을 가져온다.
+		// key : 가져올 요소의 키
+		// return : 가져올 요소의 값
+		
+		return "";
+	}
+	
+	public int size() {
+		// 요소의 개수를 반환한다.
+		// return : 요소의 개수
+		
+		return 0;
+	}
+	
+//	public void put(String key, String value) {
+//		// 해당 키의 값을 요소로 수정한다.
+//		// key : 수정할 요소의 키
+//		// value : 수정할 요소의 값
+//	}
+	
+	public void remove(String key) {
+		// 원하는 키의 요소를 삭제한다.
+		// key : 삭제할 요소의 키
+	}
+	
+	public boolean containKey(String key) {
+		// 해당 키가 존재하는지 확인한다.
+		// key : 확인할 키
+		// return : 키의 존재 유무
+		
+		return true;
+	}
+	
+	public boolean containsValue(String value) {
+		// 해당 값이 존재하는지 확인한다.
+		// value : 확인할 값
+		// return : 값의 존재 유무
+		
+		return true;
+	}
+	
+	public void clear() {
+		// 배열의 모든 요소를 삭제한다.
+	}
+}
