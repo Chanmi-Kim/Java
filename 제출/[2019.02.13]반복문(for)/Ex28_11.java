4e520da17374fcf6cf66ac0cca9ae38094b17a8f
xxHANIxx <kimcm0319@gmail.com>
2019-04-09 14:17:44 +0900

    Add File [Ex28_11.java]
    1. JAVA 파일 수정
    
    - Ex28_11.java 파일 수정
      (제출\[2019.02.13]반복문(for)\Ex28_11.java

---
+++ 제출/[2019.02.13]반복문(for)/Ex28_11.java
@@ -0,0 +1,51 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.13.
+
+요구사항 11. 2부터 100사이의 소수를 구하시오.
+
+출력]
+
+2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+*/
+
+public class Ex28_11 {
+
+	public static void main(String[] args) {
+
+		primeNumber();
+	}
+
+	public static void primeNumber() {
+
+		for (int i = 2; i < 100; i++) {
+
+			for (int j = 2; j < i; j++) {
+
+				if (i % j != 0) {
+
+					System.out.printf("%d", i);
+
+					if (j < i) {
+
+						System.out.print(", ");
+						break;
+
+					} else {
+
+						System.out.print(" ");
+						break;
+
+					}
+
+				} else {
+					break;
+				}
+
+			}
+
+		}
+	}
+}
