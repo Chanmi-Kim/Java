0a00a2aecdbad3f56d377a865efc37fa3ff5f658
xxHANIxx <kimcm0319@gmail.com>
2019-04-09 14:18:00 +0900

    Add File [Ex28_12.java]
    1. JAVA 파일 수정
    
    - Ex28_12.java 파일 수정
      (제출\[2019.02.13]반복문(for)\Ex28_12.java

---
+++ 제출/[2019.02.13]반복문(for)/Ex28_12.java
@@ -0,0 +1,33 @@
+﻿/*
+
+작성자 : xxHANIxx
+작성일자 : 2019.02.13.
+
+요구사항 12. 1부터 100사이의 완전수를 구하시오.
+
+출력]
+
+6 = [1, 2, 3]
+28 = [1, 2, 4, 7, 14]
+
+완전수 : 자기 자신을 제외한 나머지 약수들의 합이 자신과 동일한 수
+
+*/
+
+public class Ex28_12 {
+
+	public static void main(String[] args) {
+		
+	    for(int i=1;i<=100;i++) {
+            int sum=0;
+            for(int j=1;j<i;j++) {
+                if(i%j==0)
+                    sum+=j;
+            }
+            if(sum==i)
+                System.out.println(i);
+        }
+		
+	}
+	
+}
