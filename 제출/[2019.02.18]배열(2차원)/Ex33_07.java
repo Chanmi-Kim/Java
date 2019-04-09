e467c51d9c9f0985311ed1d0d6e73c173e40547f
xxHANIxx <kimcm0319@gmail.com>
2019-04-09 15:30:55 +0900

    Add File [Ex33_07.java]
    1. JAVA 파일 추가
    
    - Ex33_07.java 파일 수정
      (제출\[2019.02.18]배열(2차원)\Ex33_07.java)

---
+++ 제출/[2019.02.18]배열(2차원)/Ex33_07.java
@@ -0,0 +1,73 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex33_07 {
+
+	public static void main(String[] args) throws Exception {
+		
+		output();
+
+	}
+
+	private static void output() throws Exception {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("행과 열의 갯수 : ");
+		int line = Integer.parseInt(reader.readLine());	
+		
+		int n = 1;
+		
+		int[][] nums = new int[line][line];
+		
+		for (int i=0; i<nums.length-1; i++) { //행
+			
+			int sum = 0;
+			
+			for (int j=0; j<nums[0].length; j++) { //각 열의 4열까지 수를 배치하고, 5열에 sum값 배치
+				
+				if (j == nums[0].length-1) {
+					nums[i][j] = sum;
+					sum = 0;
+					break;
+				}
+				
+				nums[i][j] = n;
+				sum += n;
+				n++;
+				
+			}
+			
+		}
+		
+		//5행 출력
+		int sum_line = nums.length - 1; //기준 행
+		
+		for (int i=0; i<nums.length; i++) { //열
+			
+			int sum = 0;
+			
+			for (int j=0; j<nums[0].length; j++) {
+				sum += nums[j][i];
+			}
+			
+			nums[sum_line][i] = sum;
+			
+		}
+		
+		print(nums);
+		
+	}//output
+
+	private static void print(int[][] nums) {
+		
+		for (int i=0; i<nums.length; i++) {
+			for (int j=0; j<nums[0].length; j++) {
+				System.out.printf("%5d", nums[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}//print
+
+}
