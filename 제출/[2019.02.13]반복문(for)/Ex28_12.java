40026baa35f1f3c8219aaa4f7f6488179901a81e
xxHANIxx <kimcm0319@gmail.com>
2019-04-09 15:18:58 +0900

    Modify File [Ex28_12.java]
    1. JAVA 파일 수정
    
    - Ex28_12.java 파일 수정
      (제출\[2019.02.13]반복문(for)\Ex28_12.java)

--- 제출/[2019.02.13]반복문(for)/Ex28_12.java
+++ 제출/[2019.02.13]반복문(for)/Ex28_12.java
@@ -17,17 +17,29 @@
 public class Ex28_12 {
 
 	public static void main(String[] args) {
-		
-	    for(int i=1;i<=100;i++) {
-            int sum=0;
-            for(int j=1;j<i;j++) {
-                if(i%j==0)
-                    sum+=j;
-            }
-            if(sum==i)
-                System.out.println(i);
-        }
-		
+
+		for (int i = 1; i <= 100; i++) {
+
+			String txt = "";
+			int sum = 0;
+
+			for (int j = 1; j < i; j++) {
+
+				if (i % j == 0) {
+
+					txt += j;
+					sum += j;
+					if (sum != i) {
+						txt += ", ";
+					} else {
+						continue;
+					}
+				}
+			}
+
+			if (sum == i) {
+				System.out.printf("%2d = [%s]\n", sum, txt);
+			}
+		}
 	}
-	
 }
